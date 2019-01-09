3c77f6020b9a1737822c210dc6c60d7d314e1419
isabelmaccabee <isabel.maccabee@gmail.com>
2019-01-09 16:17:22 +0000

    create back button AR screen

--- screens/AR.js
+++ screens/AR.js
@@ -11,7 +11,8 @@ import {
   // AppRegistry,
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  TouchableHighlight
 } from 'react-native';
 
 import { ViroARSceneNavigator } from 'react-viro';
@@ -44,10 +45,22 @@ export default class ViroSample extends Component {
   // Returns the ViroARSceneNavigator which will start the AR experience
   _getARNavigator() {
     const { sharedProps } = this.state;
+    const { navigation } = this.props;
 
     return (
       <View style={styles.containerView}>
         <ViroARSceneNavigator {...sharedProps} initialScene={{ scene: InitialARScene }} />
+        <View style={styles.buttonView}>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => {
+              navigation.navigate('HomeScreen');
+            }}
+            underlayColor="#00000000"
+          >
+            <Text style={{ color: 'white' }}>Click ME</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -90,7 +103,17 @@ export default class ViroSample extends Component {
 const styles = StyleSheet.create({
   containerView: {
     flex: 1
-  }
+  },
+  buttonView: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    height: 50
+  },
+  button: { backgroundColor: 'blue', height: 20 }
 });
 
 module.exports = ViroSample;
