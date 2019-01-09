201d9f4b169c59d81d145784b026ef07758401df
daviidbartlett <daviidbartlett@gmail.com>
2019-01-09 10:01:53 +0000

    make Home component in Screen dir

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Home extends Component {
+  state = {};
+
+  render() {
+    return (
+      <View>
+        <Text>This is the home page.</Text>
+      </View>
+    );
+  }
+}
