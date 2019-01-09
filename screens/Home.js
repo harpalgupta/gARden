0c36455e764cefc164e5ab3b4ad8928bb2b942b3
daviidbartlett <daviidbartlett@gmail.com>
2019-01-09 10:40:14 +0000

    add button to home screen in order nav to AR

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, Button, StyleSheet
+} from 'react-native';
 
 export default class Home extends Component {
   state = {};
 
   // If this remains empty we should make it a pure function component
   render() {
+    const { navigation } = this.props;
     return (
       <View style={styles.container}>
         <Text style={styles.text}>This is the home page.</Text>
+        <Button
+          title="go to AR!"
+          onPress={() => {
+            navigation.navigate('ARScreen');
+          }}
+        />
       </View>
     );
   }
