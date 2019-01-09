0249168853f70ef4241d9eb2825541a2898575dc
daviidbartlett <daviidbartlett@gmail.com>
2019-01-09 10:18:08 +0000

    create switch navigator in App.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class Home extends Component {
   state = {};
 
+  // If this remains empty we should make it a pure function component
   render() {
     return (
-      <View>
-        <Text>This is the home page.</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>This is the home page.</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 40
+  }
+});
