5b57c240d74e72258c89c81baf62224a80463e0d
isabelmaccabee <isabel.maccabee@gmail.com>
2019-01-09 15:47:26 +0000

    render flow using external url from database

--- js/gardenARScene.js
+++ js/gardenARScene.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 
-import { StyleSheet } from 'react-native';
+// import { StyleSheet } from 'react-native';
 
 import {
   ViroARScene, ViroNode, Viro3DObject, ViroAmbientLight, ViroConstants
@@ -10,7 +10,12 @@ import {
 
 const lavObj = require('./res/lavender/lavender_plant.obj');
 const lavMtl = require('./res/lavender/lavender_plant.mtl');
-const lavPng = require('./res/lavender/lavender_plant.png');
+
+const lavPng = {
+  uri:
+    'https://firebasestorage.googleapis.com/v0/b/test-33015.appspot.com/o/lavender_plant.png?alt=media&token=362235be-b069-49ef-a85a-d5a5423408d0'
+};
+// const lavPng = require('./res/lavender/lavender_plant.png');
 
 export default class HelloWorldSceneAR extends Component {
   constructor() {
@@ -53,7 +58,7 @@ export default class HelloWorldSceneAR extends Component {
     } = this.state;
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized}>
-        <ViroAmbientLight color="#ffffff" style={styles.helloWorldTextStyle} />
+        <ViroAmbientLight color="#ffffff" />
         <ViroNode position={[0, -1, -1]} dragType="FixedToWorld" onDrag={() => {}}>
           <Viro3DObject
             source={source}
@@ -68,14 +73,14 @@ export default class HelloWorldSceneAR extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center'
-  }
-});
+// const styles = StyleSheet.create({
+//   helloWorldTextStyle: {
+//     fontFamily: 'Arial',
+//     fontSize: 30,
+//     color: '#ffffff',
+//     textAlignVertical: 'center',
+//     textAlign: 'center'
+//   }
+// });
 
 module.exports = HelloWorldSceneAR;
