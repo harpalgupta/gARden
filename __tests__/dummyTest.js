7623d97cb8b71066cd49fd84fffd622a2331107c
isabelmaccabee <isabel.maccabee@gmail.com>
2019-01-10 18:20:12 +0000

    create utils function to aid createUniqueID function

--- __tests__/dummyTest.js
+++ __tests__/dummyTest.js
@@ -1,6 +1,12 @@
-import { createUniqueID } from '../utils/index';
+import { createUniqueID, getNumfrom65to90 } from '../utils/index';
 
 describe('createUniqueId', () => {
+  describe('getNumfrom65to90', () => {
+    it('returns a number between 65 and 90', () => {
+      expect(getNumfrom65to90()).toBeLessThan(91);
+      expect(getNumfrom65to90()).toBeGreaterThan(64);
+    });
+  });
   it('returns string of only letters and nums', () => {
     expect(/[^a-z\d]/gi.test(createUniqueID())).toEqual(false);
     expect(typeof createUniqueID()).toBe('string');
--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,15 @@
-exports.createUniqueID = () => 'stringABC';
+const utils = {};
+
+utils.getNumfrom65to90 = () => Math.floor(Math.random() * (91 - 65 + 1) + 65);
+
+utils.createUniqueID = () => {
+  const letter = String.fromCharCode(utils.getNumfrom65to90());
+  return letter;
+};
+//  65 - 90 upper
+//  97 - 122 lower
+
+//  0.1 * 650
+//  0.9* 91
+
+module.exports = utils;
