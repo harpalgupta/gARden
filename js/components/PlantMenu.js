3477c01c85a460aad8ad039442bd7958ede9b14e
harpalgupta <harpal.gupta@gmail.com>
2019-01-14 10:35:27 +0000

    make get to firestore async

--- js/components/PlantMenu.js
+++ js/components/PlantMenu.js
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 
 import PlantCard from './PlantCard';
-import { firebase } from '../../config/index';
+import api from '../api';
 
 
 class PlantMenu extends React.Component {
@@ -18,13 +18,13 @@ class PlantMenu extends React.Component {
   }
 
   componentDidMount = async () => {
-    const db = firebase.firestore();
+    this.fetchMenuItems();
+  }
 
-
-    db.collection('plants').get().then(
+  fetchMenuItems = () => {
+    api.getMenuItems().then(
       (querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          // console.log(doc.id, ' => ', doc.data());
           const { data } = this.state;
           this.setState(
             { data: [...data, { key: doc.id }] }
@@ -32,36 +32,6 @@ class PlantMenu extends React.Component {
         });
       }
     );
-
-    // const docRef = db.collection('plants').doc('lavender');
-
-    // docRef.get().then((doc) => {
-    //   if (doc.exists) {
-    //     const {
-    //       objAttr: { obj, texture, scale }
-
-    //     } = doc.data();
-    //     console.log(doc.data());
-    //   }
-    // });
-
-
-    // db.collection('plants').valueChanges().map(document => document((a) => {
-    //   const data = a.payload.doc.data();// Here is your content
-    //   const id = a.payload.doc.id;// Here is the key of your document
-    //   return { id, ...data };
-    // }));
-    // db.getCollections().then((querysnapshot) => { querysnapshot.forEach((collection) =>
-    // { console.log('<<<', collection.id); }); });
-    // const events = await firebase.firestore().collection('plants').get()
-    //   .then((querySnapshot) => {
-    //     querySnapshot.docs.map((doc, index) => {
-    //       console.log(`LOG${index}`, doc.data());
-    //       return doc.data();
-    //     });
-    //     console.log(events);
-    //   });
-    // this.setState({ data: [] });
   }
 
   render() {
@@ -70,7 +40,6 @@ class PlantMenu extends React.Component {
     return (
       <View style={styles.menu}>
         <FlatList
-          // data={[{ key: 'lavender' }, { key: 'rose' }]}
           data={data}
           renderItem={({ item }) => (
             <PlantCard plantName={item.key} addPlantToRenderList={addPlantToRenderList} />
