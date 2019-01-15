3ed630a1ff9246e40d1f883ee254c8103da571f8
tomoose11 <tomwall_84@hotmail.co.uk>
2019-01-15 15:54:05 +0000

    logo added

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {
-  View, Text, Button, StyleSheet, TouchableHighlight
+  View, Text, StyleSheet, TouchableHighlight, Image
 } from 'react-native';
+
+const logo = require('../assets/logo.png');
 
 export default class Home extends Component {
   state = {
@@ -12,8 +14,8 @@ export default class Home extends Component {
     const { navigation } = this.props;
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>This is the home page.</Text>
-        <Button
+        {/* <Text style={styles.text2}>This is the home page.</Text> */}
+        {/* <Button
           title="go to AR!"
           onPress={() => {
             navigation.navigate('ARScreen');
@@ -25,7 +27,8 @@ export default class Home extends Component {
             navigation.navigate('About');
           }}
           style={styles.button}
-        />
+        /> */}
+        <Image style={{ height: 280, width: 280, marginBottom: 20 }} source={logo} />
         <TouchableHighlight
           style={styles.button}
           onPress={() => {
@@ -54,19 +57,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(153, 238, 153, 0.329)'
+    backgroundColor: 'white'
   },
   text: {
     fontSize: 25,
     padding: 10,
     paddingRight: 30,
-    paddingLeft: 30
+    paddingLeft: 30,
+    color: 'rgba(74,111,165,0.7)'
+  },
+  text2: {
+    fontSize: 25,
+    padding: 10,
+    paddingRight: 30,
+    paddingLeft: 30,
+    marginBottom: 10,
+    color: 'rgba(74,111,165,0.7)'
   },
   button: {
     borderWidth: 1,
-    borderColor: 'green',
+    borderColor: 'rgba(74,111,165,0.5)',
     borderRadius: 100,
     backgroundColor: 'white',
-    marginBottom: '3%'
+    marginBottom: '4%'
   }
 });
