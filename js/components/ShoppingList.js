fdc89163210e888f73fdd1ee20d8022100a90978
isabelmaccabee <isabel.maccabee@gmail.com>
2019-01-16 10:57:34 +0000

    make restructureCount func so counter can be in data format for table

--- js/components/ShoppingList.js
+++ js/components/ShoppingList.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
 
 class ShoppingList extends Component {
   state = {
-    // plantsCounter: { 'English Lavender': 2, Lupin: 1 }
+    plantsCounter: { 'English Lavender': 2, Lupin: 1 }
   };
 
+  restructureCounter = obj => Object.entries(obj);
+
   render() {
-    // const {
-    //   plantsCounter
-    // } = this.state;
+    const { plantsCounter } = this.state;
     return (
-      <View>
+      <View style={styles.tableContainer}>
         <Table>
-          <Row data={['No.', 'Plant']} />
-          <Rows data={[['2', 'Lavender'], ['1', 'Banana']]} />
+          <Row data={['Plant', 'No.']} />
+          <Rows data={this.restructureCounter(plantsCounter)} />
         </Table>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  tableContainer: {
+    width: 300,
+    height: 300
+  },
+  table: {}
+});
 export default ShoppingList;
